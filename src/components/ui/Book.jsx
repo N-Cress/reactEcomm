277dc523547c8e0b87abf44dc79634322535c464
Noah <azsnobird1227@yahoo.com>
2023-09-25 13:12:19 -0500

    price, rating ui components logic more bookPage

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,39 +1,23 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
+import Rating from '../ui/Rating.jsx';
+import Price from '../ui/Price.jsx';
 
 export default function Book({ book }) {
   return (
     <div className="book">
-                        <Link to={book.id}>
-                            <figure className="book__img--wrapper">
-                                <img src={book.url} alt="" />
-                            </figure>
-                        </Link>
-                        <div className="book__title">
-                            <Link to={book.id} className='book__title--link'> 
-                               {book.title}
-                            </Link>
-                        </div>
-                        <div className="book__ratings">
-                            {
-                                new Array(Math.floor(book.rating)).fill(0).map((_, index) => <FontAwesomeIcon icon="star"> </FontAwesomeIcon>) 
-                            } {
-                                Number.isInteger(!book.rating) && <FontAwesomeIcon icon="star-half-alt"></FontAwesomeIcon>
-                            }
-                        </div>
-                        <div className="book__price"> 
-                        { book.salePrice ? (
-                            <>
-                            <span className="book__price--normal"> ${book.originalPrice}</span>
-                           ${book.salePrice}
-                            </>
-                        ) : (
-                           <>
-                           ${book.originalPrice}
-                           </> 
-                        )}
-                        </div>
-                    </div>
+        <Link to={book.id}>
+            <figure className="book__img--wrapper">
+                <img src={book.url} alt="" />
+            </figure>
+        </Link>
+        <div className="book__title">
+            <Link to="/books/1" className='book__title--link'> 
+                {book.title}
+            </Link>
+        </div>
+        <Rating rating={book.rating} /> 
+        <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+    </div>
   )
 }
--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
+import Rating from '../components/ui/Rating.jsx';
+import Price from '../components/ui/Price.jsx';
 
 export default function BooksPage({ books }) {
   return (
@@ -12,7 +14,21 @@ export default function BooksPage({ books }) {
                         <Link to="/books" className="book__link">
                             <FontAwesomeIcon icon="arrow-left" /> 
                         </Link>
-                        
+                        <Link to="/books"> 
+                            <h2 className='book__selected--title--top'> Books </h2>
+                        </Link>
+                    </div>
+                    <div className="book__selected">
+                        <figure className="book__selected--figure">
+                            <img src="https://covers.openlibrary.org/b/id/10958382-L.jpg" alt="" />
+                        </figure>
+                        <div className="book__selected--description">
+                            <h2 className="book__selected--title"> </h2>
+                            <Rating rating="4.5" />
+                            <div className="book__selected--price">
+                                <Price originalPrice={10} salePrice={null} />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
